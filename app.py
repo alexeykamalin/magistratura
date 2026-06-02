630bab18b77ff8c2f2d1b604a745e1797ffd2fa2
Alise <alise@mail.ru>
2026-06-02 21:31:19 +0300

    last vercion

--- app.py
+++ app.py
@@ -1,52 +1,113 @@
-from flask import Flask, render_template, request, g, jsonify
+from flask import Flask, render_template, request, g, jsonify, session, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = 'magistratura'
+
+
+def get_all_users():
+    cursor = g.conn.cursor()
+    cursor.execute('SELECT * FROM users')
+    data = cursor.fetchall()
+    cursor.close()
+    return data
+
+def get_all_messages(user1, user2):
+    cursor = g.conn.cursor()
+    cursor.execute('SELECT * FROM messages WHERE ownerId=%s AND deliverId=%s OR deliverId=%s AND ownerId=%s',(user1,user2,user1,user2))
+    data = cursor.fetchall()
+    cursor.close()
+    return data
 
 @app.before_request
 def connect():
     conn = mysql.connector.connect(
-        host="185.114.247.43", 
-        database="sch688_magistratura", 
-        user="sch688_magistratura", 
+        host="185.114.247.43",
+        database="sch688_magistratura",
+        user="sch688_magistratura",
         password="Qwerty123")
     g.conn = conn
-    
+
 @app.teardown_request
 def close_connect(er):
     g.conn.close()
-    
+
 @app.route("/")
 def index():
-    return render_template('registration.html')
+    return render_template("registration.html")
 
-@app.route("/login")
+@app.route("/enter")
 def registration():
-    return render_template('avtorization.html')
+    return render_template("avtorization.html")
 
-@app.route("/user_registration", methods=['POST'])
+@app.route("/chat")
+def profile():
+    if 'user_id' in session:
+        cursor = g.conn.cursor()
+        id_ = session['user_id']
+        cursor.execute('SELECT * FROM users WHERE id=%s',(id_,))
+        data = cursor.fetchone()
+        all_users = get_all_users()
+        user2_id = request.args.get('id')
+        cursor.execute('SELECT * FROM users WHERE id=%s',(user2_id,))
+        user2 = cursor.fetchone()
+        messages = get_all_messages(id_, user2_id)
+        print(messages)
+        cursor.close()
+        return render_template("chat.html", 
+                               owner=data, 
+                               deliver=user2, 
+                               all_users=all_users, 
+                               messages=messages)
+    else:
+        return render_template("avtorization.html")
+
+@app.route("/user_registration", methods=["POST"])
 def user_registration():
     data = request.json
     cursor = g.conn.cursor()
-    cursor.execute("INSERT INTO users (name, surname, login, password) VALUES (%s, %s, %s, %s)", 
-                   (data['name'], data['surname'], data['login'], data['password']))
+    cursor.execute("INSERT INTO users (name, surname, login, password) VALUES (%s,%s,%s,%s)",
+                   (data["name"], data["surname"], data["login"], data["password"]))
     g.conn.commit()
     new_user_id = cursor.lastrowid
     cursor.close()
-    return jsonify({'user_id': new_user_id, 'code': 200})
+    return jsonify({"user_id": new_user_id, "code": 200})
 
-@app.route("/user_avtorization", methods=['POST'])
-def user_registration():
+@app.route("/user_avtorization", methods=["POST"])
+def user_avtorization():
+    data = request.json
+    login = data['login']
+    pas = data['password']
+    cursor = g.conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE login=%s",(login,))
+    user = cursor.fetchall()
+    cursor.close()
+    if user:
+        if pas == user[0][4]:
+            session['user_login'] = user[0][3]
+            session['user_id'] = user[0][0]
+            return jsonify({"result": True,"message": 'avtorization ok', "code": 200}) 
+        else:
+            return jsonify({"result": False,"message": 'wrong password', "code": 400})
+    else:
+        return jsonify({"result": False,"message": 'user not in bd', "code": 400})
+
+@app.route("/send_mes", methods=["POST"])
+def send_mes():
     data = request.json
     cursor = g.conn.cursor()
-    cursor.execute("SELECT * from users WHERE login=%s",(data['login'],))
-    user = cursor.fetch()
+    cursor.execute("INSERT INTO `messages` (`ownerId`, `deliverId`, `text`) VALUES (%s, %s, %s)",
+                   (data["owner"], data["deliver"], data["text"]))
+    g.conn.commit()
+    new_mes_id = cursor.lastrowid
     cursor.close()
-    return jsonify({'user': user, 'code': 200})
+    return jsonify({"result": True, "mes_id": new_mes_id, "code": 200})
 
-@app.route("/chat")
-def chat():
-    name = request.args.get('nickname')
-    return f'{name}, привет!'
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    session.pop('user_login', None)
+    return redirect(url_for('/enter'))
 
 app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -13,4 +13,44 @@ $(document).ready(function(){
             })
         })
     })
+
+    $('#avtorizationForm').on('submit',function(e){
+        e.preventDefault();
+        $.ajax({
+            url: '/user_avtorization',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                login: $('#login').val(),
+                password: $('#password').val(),
+            })
+        }).done(function(data){
+            if (data.result){
+                window.location.href = '/chat';
+            }else{
+                alert('Что-то пошло не так');
+            }
+        })
+    })
+
+    $('#send_mes').on('click',function(){
+        
+        var urlParams = new URLSearchParams(window.location.search);
+        var owner = urlParams.get('owner');
+        var deliver = urlParams.get('id');
+        $.ajax({
+            url: '/send_mes',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                owner: owner,
+                deliver: deliver,
+                text: $('#mes_input').val()
+            })
+        }).done(function(data){
+            if (data.result){
+                window.location.reload();
+            }
+        })
+    })
 })
